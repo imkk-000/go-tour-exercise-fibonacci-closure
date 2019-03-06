e9fce314b4aa7f99649aa2de4a3cff11ee8514c9
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-03-06 23:31:35 +0700

    [Edited]: store old value with append value to slice

--- main.go
+++ main.go
@@ -5,22 +5,14 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	var counter, resultNumber int
-	f := [2]int{0, 1}
+	counter := -1
+	f := []int{0, 1}
 	return func() int {
-		switch counter {
-		case 0:
-			resultNumber = f[0]
-		case 1:
-			resultNumber = f[1]
-		default:
-			temp := f[1]
-			f[1] = f[0] + temp
-			f[0] = temp
-			resultNumber = f[1]
+		if counter >= 1 {
+			f = append(f, f[counter]+f[counter-1])
 		}
 		counter++
-		return resultNumber
+		return f[counter]
 	}
 }
 
